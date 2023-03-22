b6eac63cf97ad4edbc10bcd0a972592919f2c08f
walidferhat89 <walidferhat89@gmail.com>
2023-03-22 14:07:52 -0400

    updated test1

--- src/main/java/pages/gitDemo.java
+++ src/main/java/pages/gitDemo.java
@@ -5,5 +5,8 @@ public class gitDemo {
 
     public static void main(String[] args) {
         System.out.println("i created a new branch");
+        System.out.println("Hi just testing mergin from my branch to master");
     }
+
+
 }
